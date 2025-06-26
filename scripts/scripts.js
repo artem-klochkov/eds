cc57dfe06be432259798b887ae5c3b6cae63e869
artem-klochkov <artem.klochkov@axamit.com>
2025-06-26 14:15:16 +0400

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -138,17 +138,9 @@ function loadDelayed() {
   // load anything that can be postponed to the latest here
 }
 
-function initAlloy() {
-  !function(n,o){o.forEach(function(o){n[o]||((n.__alloyNS=n.__alloyNS||
-    []).push(o),n[o]=function(){var u=arguments;return new Promise(
-    function(i,l){n[o].q.push([i,l,u])})},n[o].q=[])})}
-    (window,["alloy"]);
-}
-
 async function loadPage() {
   await loadEager(document);
   await loadLazy(document);
-  initAlloy();
   loadDelayed();
 }
 
